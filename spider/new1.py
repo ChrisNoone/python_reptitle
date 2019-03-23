9b01919647e5b2fb9f17fe97d32e9ae9ea39f56b
NooneLiu <lv8023_chao@163.com>
2019-03-23 12:15:47 +0800

    reptile

--- spider/new1.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-
-import urllib.request
-
-req = urllib.request.Request("http://www.baidu.com")
-response = urllib.request.urlopen(req)
-fp = open("D://main1.html", "w+")
-fp.write(response.read())
-fp.close()
