733eeb1fb11ea7ffe0dd428a160ed70070fde211
NooneLiu <lv8023_chao@163.com>
2019-03-25 15:23:18 +0800

    reptile

---
+++ python3.x/web_parse.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+
+data = []
+with open('../week1/1_2/1_2code_of_video/web/new_index.html', 'r') as web_data:
+    Soup = BeautifulSoup(web_data, 'lxml')
+    titles = Soup.select('ul > li > div.article-info > h3 > a')
+    images = Soup.select('ul > li > img')
+    descs = Soup.select('ul > li > div.article-info > p.description')
+    rates = Soup.select('ul > li > div.rate > span')
+    cates = Soup.select('ul > li > div.article-info > p.meta-info')
+
+# print(titles, images, descs, rates, cates, sep='\n-------------------------\n')
+for title, image, desc, rate, cate in zip(titles, images, descs, rates, cates):
+    info = {
+        'title': title.get_text(),
+        'image': image.get('src'),
+        'desc': desc.get_text(),
+        'rate': rate.get_text(),
+        'cate': list(cate.stripped_strings)
+    }
+    data.append(info)
+
+for i in data:
+    if float(i['rate']) > 3:
+        print(i['title'], i['cate'])
