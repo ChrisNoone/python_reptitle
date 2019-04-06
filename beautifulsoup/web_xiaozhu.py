9c1c78c7a8e3d6224bd90eecb71a8a7bcb2efe9e
NooneLiu <lv8023_chao@163.com>
2019-04-06 11:23:06 +0800

    beautiful soup

--- beautifulsoup/web_xiaozhu.py
+++ beautifulsoup/web_xiaozhu.py
@@ -1,16 +1,35 @@
 # coding: utf-8
 
 from bs4 import BeautifulSoup
-import requests
+from urllib import request, error
 import time
+import random
+
+
+def build_proxy_opener():
+    proxy_list = ['68.107.176.152:80', '203.144.174.111:8080']
+    item = random.randint(0, len(proxy_list)-1)
+    proxy = {'proxy': proxy_list[item]}
+    proxy_handler = request.ProxyHandler(proxy)
+    opener = request.build_opener(proxy_handler)
+    request.install_opener(opener)
 
 
 def get_url(page_url):
     data = []
-    web_data = requests.get(page_url)
-    time.sleep(2)
-    Soup = BeautifulSoup(web_data.text, 'lxml')
-    details = Soup.select('#page_list > ul > li > a')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
+    }
+    build_proxy_opener()
+
+    try:
+        req = request.Request(page_url, headers=headers)
+        web_data = request.urlopen(req).read().decode()
+        time.sleep(2)
+        Soup = BeautifulSoup(web_data, 'lxml')
+        details = Soup.select('#page_list > ul > li > a')
+    except (error.HTTPError, error.URLError) as e:
+        print(e)
 
     for detail in details:
         info = {
@@ -21,11 +40,21 @@ def get_url(page_url):
 
 
 def get_data(page_url):
-    web_data = requests.get(page_url)
-    time.sleep(2)
-    Soup = BeautifulSoup(web_data.text, 'lxml')
-    titles = Soup.select('div.con_l > div.pho_info > h4 > em')
-    addrs = Soup.select('div.con_l > div.pho_info > p > span')
+    print(page_url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
+    }
+    build_proxy_opener()
+
+    try:
+        req = request.Request(page_url, headers=headers)
+        web_data = request.urlopen(req).read().decode()
+        time.sleep(2)
+        Soup = BeautifulSoup(web_data, 'lxml')
+        titles = Soup.select('div.con_l > div.pho_info > h4 > em')
+        addrs = Soup.select('div.con_l > div.pho_info > p > span')
+    except (error.HTTPError, error.URLError) as e:
+        print(e)
 
     for title, addr in zip(titles, addrs):
         info = {
@@ -35,12 +64,14 @@ def get_data(page_url):
         home.append(info)
 
 
-home = []
-for i in range(1, 3):
-    url = 'http://bj.xiaozhu.com/search-duanzufang-p' + str(i) + '-0/'
-    pages = get_url(url)
-    for j in pages:
-        get_data(j['detail'])
+if __name__ == '__main__':
+    home = []
+    for i in range(1, 3):
+        url = 'http://bj.xiaozhu.com/search-duanzufang-p' + str(i) + '-0/'
+        print(url)
+        pages = get_url(url)
+        for j in pages:
+            get_data(j['detail'])
 
-for em in home:
-    print('Title：', em['title'], 'Addr：', em['addr'])
+    for em in home:
+        print('Title：', em['title'], 'Addr：', em['addr'])
--- spider/v1.py
+++ spider/v1.py
@@ -1,11 +1,28 @@
 # coding: utf-8
 
 from urllib import request
+import requests
 import os
 
 
+def def_requests(url):
+    rsp = requests.get(url)
+    return rsp
+
+
+def def_request(url):
+    rsp = request.urlopen(url)
+    return rsp
+
+
 if __name__ == '__main__':
-    rsp = request.urlopen('https://www.baidu.com')
-    html = rsp.read().decode()
+    base_url = 'https://www.baidu.com'
+    html1 = def_requests(base_url).text
+    html2 = def_request(base_url).read().decode()
+    print('requests：', type(html1))
+    print('request：', type(html2))
+
+    '''
     file_name = os.path.join(r'.\down_pic', 'baidu.html')
     print(file_name)
+    '''
